b2cc77c9ee6fa55b9790706adf873122acdf250d
Manishakahar <manisha.kahar1998@gmail.com>
2021-11-23 21:42:26 +0530

    [Manisha]: Added App File

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,6 @@ import java.sql.SQLOutput;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello using maven");
+        System.out.println("Hello using maven ");
     }
 }
