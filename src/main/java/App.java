06359d6666cf2b85ba3bb1346c82cb48135226c0
Manishakahar <manisha.kahar1998@gmail.com>
2021-11-23 22:04:32 +0530

    [Manisha]: Added anothe file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello using maven ");
         System.out.println("Hii");
+        System.out.println("Hello World");
 
     }
 }
